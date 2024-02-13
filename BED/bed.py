57363c500c0452e687ef680786c974f6707d717e
Sebastian Mežnar <smeznar@gmail.com>
2024-02-13 16:15:47 +0100

    Finished the script/class for calculation of BED. Nothing is tested yet.

--- BED/bed.py
+++ BED/bed.py
@@ -1,16 +1,21 @@
+import itertools
+
 import numpy as np
 from scipy.stats.qmc import LatinHypercube
+from scipy.stats import wasserstein_distance
 
 from utils import infix_to_postfix
 from evaluation import RustEval
 
 
 class BED:
-    def __init__(self, expressions, x_bounds, const_bounds, points_sampled=64, consts_sampled=16, x=None, infix=False,
-                 randomized=False, cutoff_threshold=1e20, seed=None):
+    def __init__(self, expressions, x_bounds, const_bounds, points_sampled=64, consts_sampled=16, expressions2=None,
+                 x=None, infix=False, randomized=False, cutoff_threshold=1e20, default_distance=1e10, seed=None):
         self.points_sampled = points_sampled
         self.consts_sampled = consts_sampled
         self.cutoff_threshold = cutoff_threshold
+        self.default_distance = default_distance
+
         if seed is None:
             self.seed = np.random.randint(-10000000, 10000000)
         else:
@@ -20,6 +25,10 @@ class BED:
         # Transform expressions to the postfix notation, if they are in the infix notation
         if infix:
             expressions = infix_to_postfix(expressions)
+            if expressions2 is not None:
+                expressions2 = infix_to_postfix(expressions2)
+        self.expressions = expressions
+        self.expressions2 = expressions2
 
         # Prepare data points on which the distance will be calculated
         self.randomized = randomized
@@ -27,10 +36,22 @@ class BED:
         self.const_bounds = const_bounds
         if x is not None:
             self.x = x
+            self.y = self.evaluate_expressions_on_points(self.expressions, self.x)
+            if self.expressions2 is not None:
+                self.y2 = self.evaluate_expressions_on_points(self.expressions2, self.x)
+            else:
+                self.y2 = None
         elif randomized:
             self.x = None
+            self.y = None
+            self.y2 = None
         else:
             self.x = self.sample_x()
+            self.y = self.evaluate_expressions_on_points(self.expressions, self.x)
+            if self.expressions2 is not None:
+                self.y2 = self.evaluate_expressions_on_points(self.expressions2, self.x)
+            else:
+                self.y2 = None
 
     def sample_x(self):
         interval_length = np.array([ub - lb for (lb, ub) in self.x_bounds])
@@ -56,11 +77,64 @@ class BED:
             ys.append(y)
         return ys
 
+    def bed(self, y1, y2):
+        assert len(y1) == len(y2)
+
+        cube_distance = []
+        for i in range(len(y1)):
+            if len(y1[i]) == 0 and len(y2[i]) == 0:
+                cube_distance.append(0.0)
+            elif len(y1[i]) == 0 or len(y2[i]) == 0:
+                cube_distance.append(self.default_distance)
+            else:
+                cube_distance.append(wasserstein_distance(y1[i], y2[i]))
+
+        return np.mean(cube_distance)
+
     def calculate_distances(self):
-        if self.randomized:
-            self.seed += 1
-            x = self.sample_x()
+        # Case where we try to calculate the distance between all expressions
+        if self.expressions2 is None:
+            # We only have two expression
+            if len(self.expressions) == 2:
+                if self.randomized:
+                    x = self.sample_x()
+                    y = self.evaluate_expressions_on_points(self.expressions, x)
+                    return self.bed(y[0], y[1])
+                else:
+                    return self.bed(self.y[0], self.y[1])
+
+            # We have more expressions
+            dm = np.zeros((len(self.expressions), len(self.expressions)))
+            for i, j in itertools.combinations(range(len(self.expressions)), 2):
+                if self.randomized:
+                    self.seed += 1
+                    x = self.sample_x()
+                    y = self.evaluate_expressions_on_points([self.expressions[i], self.expressions[j]], x)
+                    dm[i, j] = dm[j, i] = self.bed(y[0], y[1])
+                else:
+                    dm[i, j] = dm[j, i] = self.bed(self.y[i], self.y[j])
+
+        # Case where we try to calculate the distance between two sets of expressions
         else:
-            x = self.x
+            # We only have two expressions
+            if len(self.expressions) == 1 and len(self.expressions2) == 1:
+                if self.randomized:
+                    x = self.sample_x()
+                    y = self.evaluate_expressions_on_points([self.expressions[0], self.expressions2[0]], x)
+                    return self.bed(y[0], y[1])
+                else:
+                    return self.bed(self.y[0], self.y2[0])
 
+            # We have more expressions
+            dm = np.zeros((len(self.expressions), len(self.expressions2)))
+            for i in range(len(self.expressions)):
+                for j in range(len(self.expressions2)):
+                    if self.randomized:
+                        self.seed += 1
+                        x = self.sample_x()
+                        y = self.evaluate_expressions_on_points([self.expressions[i], self.expressions2[j]], x)
+                        dm[i, j] = self.bed(y[0], y[1])
+                    else:
+                        dm[i, j] = self.bed(self.y[i], self.y2[j])
 
+        return dm
