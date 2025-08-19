75e088f77425816454d6d6b8081e8b36af673a0e
sebastian <smeznar@gmail.com>
2025-08-19 07:23:40 +0200

    Changed expressions and MDS plot

--- BED/equivalence_classes.py
+++ BED/equivalence_classes.py
@@ -9,6 +9,7 @@ from sklearn.metrics import adjusted_rand_score, silhouette_score, v_measure_sco
 from sklearn.manifold import TSNE, MDS
 import time
 import zss
+import matplotlib as mpl
 
 from BED.utils import expr_to_zss
 from bed import BED
@@ -140,7 +141,7 @@ def factor(expr_tree: Node) -> Node:
         else:
             return Node("*", expr_tree.left, Node("2"))
     return expr_tree
-
+0
 
 def generate_subtrees(sl):
     while True:
@@ -237,8 +238,11 @@ def generate_equivalent(expr, num_equivalent, symbol_library, length_limit=40):
     return equivalent_expressions
 
 
-def show_TSNE_clusters(distance_matrix, colors, markers, exprs, num):
-    tsne = TSNE(n_components=2, metric='precomputed', init="random")
+def show_TSNE_clusters(distance_matrix, colors, markers, exprs, num, precomputed=True):
+    if precomputed:
+        tsne = TSNE(n_components=2, metric='precomputed', init="random")
+    else:
+        tsne = TSNE(n_components=2, init="random")
     embedding = tsne.fit_transform(distance_matrix)  # shape: (N, 2)
     # Step 3: Visualize
     plt.figure(figsize=(12, 8))
@@ -253,20 +257,79 @@ def show_TSNE_clusters(distance_matrix, colors, markers, exprs, num):
     plt.show()
 
 
-def show_MDS_clusters(distance_matrix, colors, markers, exprs, num):
-    mds = MDS(n_components=2, dissimilarity='precomputed')
-    embedding = mds.fit_transform(distance_matrix)  # shape: (N, 2)
-    # Step 3: Visualize
-    plt.figure(figsize=(12, 8))
-    for i in range(distance_matrix.shape[0]//num):
-        plt.scatter(embedding[(i*num):((i+1)*num), 0], embedding[(i*num):((i+1)*num), 1], color=colors[(i*num)], marker=markers[(i*num)], label=exprs[(i*num)])
-    plt.title("MDS with Precomputed Distances")
-    plt.xlabel("Component 1")
-    plt.ylabel("Component 2")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.legend()
+def show_MDS_clusters(distance_matrix, colors, markers, exprs, num, precomputed=True):
+    # High-quality, publication-ready style
+    plt.style.use('seaborn-v0_8-whitegrid')
+    mpl.rcParams.update({
+        'font.size': 14,
+        'axes.labelsize': 18,
+        'axes.titlesize': 20,
+        'xtick.labelsize': 14,
+        'ytick.labelsize': 14,
+        'legend.fontsize': 12,
+        'figure.dpi': 300,
+        'savefig.dpi': 600,   # For very sharp output
+        'pdf.fonttype': 42,   # Editable text in Illustrator
+        'ps.fonttype': 42
+    })
+
+    # Perform MDS
+    if precomputed:
+        mds = MDS(n_components=2, dissimilarity='precomputed', n_init=10, random_state=42)
+    else:
+        mds = MDS(n_components=2, n_init=10, random_state=42)
+    embedding = mds.fit_transform(distance_matrix)
+
+    # Create figure
+    fig, ax = plt.subplots(figsize=(10, 6), constrained_layout=True)
+
+    # Group plotting
+    seen_labels = set()
+    for i in range(0, distance_matrix.shape[0], num):
+        group_label = exprs[i]
+        if group_label not in seen_labels:
+            ax.scatter(
+                embedding[i:i+num, 0],
+                embedding[i:i+num, 1],
+                color=colors[i],
+                marker=markers[i],
+                s=70,
+                edgecolors='black',
+                linewidths=0.6,
+                alpha=0.85,
+                label=group_label
+            )
+            seen_labels.add(group_label)
+
+    # Titles and labels
+    ax.set_title("MDS Visualization of Expression Distances", pad=15, weight='bold')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+
+    # Legend formatting
+    legend = ax.legend(
+        title="Expression Groups",
+        bbox_to_anchor=(1.05, 1),  # Move legend outside plot
+        loc="upper left",
+        fontsize=10,
+        title_fontsize=12,
+        ncol=1,  # Compact into two columns
+        borderaxespad=0.
+    )
+    legend._legend_box.align = "left"
+    plt.setp(legend.get_title(), fontsize=13, weight='bold')
+
+    # Professional touches
+    ax.grid(True, linestyle='--', alpha=0.5)
+    ax.set_aspect('equal', adjustable='datalim')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    for spine in ax.spines.values():
+        spine.set_visible(True)
     plt.show()
+
 
 
 if __name__ == '__main__':
@@ -315,26 +378,33 @@ if __name__ == '__main__':
     #         ("log ( C * X_0 ^2 + C * X_1 ^2 + 1 )", "brown", "s"),
 
     expressions = [
-        ("C", "blue", "o"),  # constant
+        # constant
+        ("C", "blue", "o"),
 
-        ("C + C * X_0", "green", "o"),  # linear
-        ("C + C * X_1", "green", "s"),
-        ("C + C * X_0 + C * X_1", "green", "^"),
+        # linear
+        ("C * X_0", "green", "o"),
+        ("C * X_1", "green", "s"),
+        ("C + C * X_1", "green", "^"),
+        ("C + C * X_0 + C * X_1", "green", "v"),
 
-        ("C + C * X_0 + C * X_1 ^2", "red", "o"),
-        ("C + C * X_1 + C * X_0 ^2", "red", "s"),
-        ("C + C * X_0 + C * X_1 + C * X_0 * X_1 + C * X_0 ^2 + C * X_1 ^2", "red", "^"),
+        # quadratic
+        ("C * X_0 ^ 2", "red", "o"),
+        ("C + C * X_0 * X_1", "red", "s"),
+        ("C * X_0 ^ 2 + C * X_1 ^ 2", "red", "^"),
 
+        # trigonometric
         ("C * sin ( X_0 )", "purple", "o"),
-        ("C * cos ( X_1 )", "purple", "s"),
-        ("sin ( C * X_1 ) + C * cos ( X_0 )", "purple", "^"),
-        ("C * sin ( X_1 ) + cos ( C * X_0 )", "purple", "v"),
+        ("cos ( C * X_1 )", "purple", "s"),
+        ("C * sin ( X_0 ) + C * cos ( X_1 )", "purple", "^"),
 
-        ("C + sqrt ( C * X_0 )", "orange", "o"),
-        ("C + sqrt ( C * X_0 ) + sqrt ( C * X_1 )", "orange", "s"),
+        # containing square root
+        ("sqrt ( C * X_0 )", "orange", "o"),
+        ("C + sqrt ( X_0 + X_1 )", "orange", "s"),
 
-        ("log ( C * X_0 ^2 + 1 )", "brown", "o"),  # logarithmic
-        ("log ( C * X_0 ^2 + C * X_1 ^2 + 1 )", "brown", "s"),
+        # logarithmic
+        ("C * log ( X_0 )", "brown", "o"),
+        ("log ( X_1 + C )", "brown", "s"),
+        ("C * log ( X_0 * X_1 )", "brown", "^"),
     ]
     expressions = [(expression[0].split(" "), expression[1], expression[2]) for expression in expressions]
     ground_truth = []
@@ -371,8 +441,8 @@ if __name__ == '__main__':
     print("Fowlkes-Mallows: ", fowlkes_mallows_score(clusters, np.array(ground_truth)))
 
     # bed = np.log10(bed+1)
-    # show_TSNE_clusters(bed, colors, markers, labels, num_equivalent)
-    # show_MDS_clusters(bed, colors, markers, labels, num_equivalent)
+    show_TSNE_clusters(bed, colors, markers, labels, num_equivalent)
+    show_MDS_clusters(bed, colors, markers, labels, num_equivalent)
 
     t_start = time.time()
     bed = BED(all_expressions, [[1,5],[1,5]], normalize=False).calculate_distances()
@@ -390,6 +460,7 @@ if __name__ == '__main__':
     print()
     print("BED + MDS")
     bed = MDS(dissimilarity="precomputed").fit_transform(bed)
+    # print(time.time() - t_start)
     clusters = AgglomerativeClustering(n_clusters=len(expressions), linkage="single").fit_predict(bed)
     np.fill_diagonal(bed, 0)
     print("ARI BED", adjusted_rand_score(clusters, np.array(ground_truth)))
@@ -397,8 +468,8 @@ if __name__ == '__main__':
     print("V-measure: ", v_measure_score(clusters, np.array(ground_truth)))
     print("Fowlkes-Mallows: ", fowlkes_mallows_score(clusters, np.array(ground_truth)))
 
-    # show_TSNE_clusters(bed, colors, markers, labels, num_equivalent)
-    # show_MDS_clusters(bed, colors, markers, labels, num_equivalent)
+    show_TSNE_clusters(bed, colors, markers, labels, num_equivalent, precomputed=False)
+    show_MDS_clusters(bed, colors, markers, labels, num_equivalent, precomputed=False)
 
     edit = np.zeros((len(all_expressions), len(all_expressions)))
     for i in range(len(all_expressions)):
@@ -413,8 +484,8 @@ if __name__ == '__main__':
     print("Silhouette: ", silhouette_score(edit, clusters, metric="precomputed"))
     print("V-measure: ", v_measure_score(clusters, np.array(ground_truth)))
     print("Fowlkes-Mallows: ", fowlkes_mallows_score(clusters, np.array(ground_truth)))
-    # show_TSNE_clusters(edit, colors, markers, labels, num_equivalent)
-    # show_MDS_clusters(edit, colors, markers, labels, num_equivalent)
+    show_TSNE_clusters(edit, colors, markers, labels, num_equivalent)
+    show_MDS_clusters(edit, colors, markers, labels, num_equivalent)
 
     zss_exprs = []
     for expr in all_expressions:
@@ -432,5 +503,5 @@ if __name__ == '__main__':
     print("Silhouette: ", silhouette_score(tree_edit, clusters, metric="precomputed"))
     print("V-measure: ", v_measure_score(clusters, np.array(ground_truth)))
     print("Fowlkes-Mallows: ", fowlkes_mallows_score(clusters, np.array(ground_truth)))
-    # show_TSNE_clusters(edit, colors, markers, labels, num_equivalent)
-    # show_MDS_clusters(edit, colors, markers, labels, num_equivalent)
+    show_TSNE_clusters(edit, colors, markers, labels, num_equivalent)
+    show_MDS_clusters(edit, colors, markers, labels, num_equivalent)
